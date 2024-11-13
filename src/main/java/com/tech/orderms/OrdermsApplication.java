d1ffada555a781dbd19c374c3e095c4e65f09810
PC <adailson65@gmail.com>
2024-11-13 15:35:41 -0300

    first commit

---
+++ src/main/java/com/tech/orderms/OrdermsApplication.java
@@ -0,0 +1,16 @@
+package com.tech.orderms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrdermsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrdermsApplication.class, args);
+		System.out.println("==============================");
+		System.out.println("==========Executando===========");
+		System.out.println("==============================");
+	}
+
+}
