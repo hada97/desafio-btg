30f985fb4c892b687340d97e3fca6007abc41418
PC <adailson65@gmail.com>
2024-11-13 23:45:04 -0300

    add dto

---
+++ src/main/java/com/tech/orderms/listener/OrderCreatedListener.java
@@ -0,0 +1,30 @@
+package com.tech.orderms.listener;
+
+
+import com.tech.orderms.listener.dto.OrderCreatedEvent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.messaging.Message;
+import org.springframework.stereotype.Component;
+import static com.tech.orderms.config.RabbitMqConfig.ORDER_CREATED_QUEUE;
+
+
+@Component
+public class OrderCreatedListener {
+
+    private final Logger logger = LoggerFactory.getLogger(OrderCreatedListener.class);
+
+    private final OrderService orderService;
+
+    public OrderCreatedListener(OrderService orderService) {
+        this.orderService = orderService;
+    }
+
+    @RabbitListener(queues = ORDER_CREATED_QUEUE)
+    public void listen(Message<OrderCreatedEvent> message){
+        logger.info("Message consumed: {}", message);
+
+        orderService.save(message.getPayload());
+    }
+}
---
+++ src/main/java/com/tech/orderms/listener/dto/OrderCreatedEvent.java
@@ -0,0 +1,6 @@
+package com.tech.orderms.listener.dto;
+
+import java.util.List;
+
+public record OrderCreatedEvent(Long codigoPedido, Long codigoCliente, List <OrderItemEvent> itens ) {
+}
